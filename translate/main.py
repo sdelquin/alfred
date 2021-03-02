59f3bad3501b0861b7448ef44df6716311003cba
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-03-02 10:52:16 +0000

    Add workflow for translations

---
+++ translate/main.py
@@ -0,0 +1,19 @@
+import sys
+
+from googletrans import Translator
+
+from workflow import Workflow
+
+wf = Workflow()
+
+src_lang, dest_lang = sys.argv[1].strip().split('-')
+text = sys.argv[2].strip().lower()
+
+translator = Translator()
+translation = translator.translate(text, src=src_lang, dest=dest_lang)
+
+for candidate in translation.__dict__()['parts'][0]['candidates']:
+    title = candidate.lower()
+    if title != text:
+        wf.newline(title=title)
+wf.send()
