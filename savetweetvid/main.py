cdceb0c2316c561b7f63576022acb06d8f7f152a
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-03-12 13:11:36 +0000

    Add workflow for savetweetvid

---
+++ savetweetvid/main.py
@@ -0,0 +1,33 @@
+import sys
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+SERVICE_URL = 'https://www.savetweetvid.com'
+tweet_url = sys.argv[1]
+
+options = Options()
+options.headless = True
+
+try:
+    driver = webdriver.Firefox(options=options)
+    driver.get(SERVICE_URL)
+
+    element = driver.find_element_by_xpath('//*[@id="form_download"]/div/input')
+    element.send_keys(tweet_url, Keys.ENTER)
+
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, '/html/body/div/div/div/div/div[2]/div/div[1]/h5')
+        )
+    )
+
+    element = driver.find_elements_by_css_selector('a.btn-download')[0]
+except Exception:
+    print('Something dit not work! 😕')
+else:
+    print(element.get_attribute('href'))
