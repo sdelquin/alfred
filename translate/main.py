625cb55743c9bb3e42adc481a15f1bb82f9ab944
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-07-22 14:22:48 +0100

    Fix issues with translation candidates

--- translate/main.py
+++ translate/main.py
@@ -12,8 +12,11 @@ text = sys.argv[2].strip().lower()
 translator = Translator()
 translation = translator.translate(text, src=src_lang, dest=dest_lang)
 
-for candidate in translation.__dict__()['parts'][0]['candidates']:
-    title = candidate.lower()
-    if title != text:
-        wf.newline(title=title)
+try:
+    for candidate in translation.__dict__()['parts'][0]['candidates']:
+        title = candidate.lower()
+        if title != text:
+            wf.newline(title=title)
+except TypeError:
+    wf.newline(title=translation.text)
 wf.send()
