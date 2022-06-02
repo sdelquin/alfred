74d2257a3c77196c56be74263bb053138c302caa
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-06-02 20:35:06 +0100

    Fix bug in unshorten

--- unshorten/main.py
+++ unshorten/main.py
@@ -7,5 +7,5 @@ import pyperclip
 shortened_url = pyperclip.paste()
 cmd = f'curl -ksLI {shortened_url}'
 output = subprocess.run(shlex.split(cmd), capture_output=True, encoding='utf-8')
-unshortened_url = re.search(r'Location: (.*)', output.stdout).groups()[0]
+unshortened_url = re.search(r'[Ll]ocation: (.*)', output.stdout).groups()[0]
 print(unshortened_url, end='')
