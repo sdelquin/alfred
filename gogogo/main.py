29fa1ebf9786e71510c9c1d55708f1554d6c19d5
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-01-21 10:31:08 +0000

    Add option to open routes

--- gogogo/main.py
+++ gogogo/main.py
@@ -1,3 +1,4 @@
+import os
 import re
 import sys
 import webbrowser
@@ -6,16 +7,19 @@ from pathlib import Path
 
 f = Path(__file__).parent / 'routes.csv'
 target = sys.argv[1].strip().lower()
-with open(f) as csvfile:
-    routes = DictReader(csvfile)
-    for route in routes:
-        regex = f'^{route["regex"]}$'
-        url = route['url']
-        if m := re.match(regex, target):
-            if len(groups := m.groups()) > 0:
-                for i, item in enumerate(groups, start=1):
-                    placeholder = f'${i}'
-                    url = url.replace(placeholder, item)
-            break
 
-webbrowser.open(url)
+if target == 'e':
+    os.system(f'open {f}')
+else:
+    with open(f) as csvfile:
+        routes = DictReader(csvfile)
+        for route in routes:
+            regex = f'^{route["regex"]}$'
+            url = route['url']
+            if m := re.match(regex, target):
+                if len(groups := m.groups()) > 0:
+                    for i, item in enumerate(groups, start=1):
+                        placeholder = f'${i}'
+                        url = url.replace(placeholder, item)
+                break
+    webbrowser.open(url)
