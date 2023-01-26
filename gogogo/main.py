0e4c03872c2d1b2879ca40cbff45ea91a27bb960
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-01-26 12:18:26 +0000

    Add icons for gogogo workflow

--- gogogo/main.py
+++ gogogo/main.py
@@ -1,29 +1,20 @@
-import os
-import re
 import sys
-import webbrowser
 from csv import DictReader
 from pathlib import Path
 
+from workflow import Workflow
+
+wf = Workflow(icons_path=Path(__file__).parent / 'img')
 f = Path(__file__).parent / 'routes.csv'
 target = sys.argv[1].strip().lower()
 
-if target == 'e':
-    os.system(f'open {f}')
-else:
-    url = None
-    with open(f) as csvfile:
-        routes = DictReader(csvfile)
-        for route in routes:
-            regex = f'^{route["regex"]}$'
-            if m := re.match(regex, target):
-                url = route['url']
-                if len(groups := m.groups()) > 0:
-                    for i, item in enumerate(groups, start=1):
-                        placeholder = f'${i}'
-                        url = url.replace(placeholder, item)
-                break
-    if url is None:
-        print('ERROR', end='')
-    else:
-        webbrowser.open(url)
+with open(f) as csvfile:
+    routes = DictReader(csvfile)
+    for route in routes:
+        alias = route["alias"]
+        if not target or alias.startswith(target):
+            url = route['url']
+            icon = f'{alias.split()[0]}.png'
+            wf.newline(title=alias, autocomplete=alias, subtitle=url, arg=url, icon=icon)
+
+wf.send()
