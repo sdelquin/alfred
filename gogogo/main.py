b90ac648fc04e284e6a58bb5f2440afdda994207
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-02-07 16:16:57 +0000

    Refactor using own entry description

--- gogogo/main.py
+++ gogogo/main.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import unicodedata
 from csv import DictReader
@@ -5,9 +6,18 @@ from pathlib import Path
 
 from workflow import Workflow
 
+
+def match(query_parts, name):
+    name_parts = re.split(r'[- ]+', name.lower())
+    for qpart in query_parts:
+        if not any(npart.startswith(qpart) for npart in name_parts):
+            return False
+    return True
+
+
 # https://bit.ly/3wBRr4e
-target = unicodedata.normalize('NFC', sys.argv[1])
-target = target.strip().lower()
+query = unicodedata.normalize('NFC', sys.argv[1]).strip().lower()
+query_parts = re.split(r'[- ]+', query)
 
 wf = Workflow(icons_path=Path(__file__).parent / 'img')
 f = Path(__file__).parent / 'routes.csv'
@@ -15,20 +25,10 @@ f = Path(__file__).parent / 'routes.csv'
 with open(f) as csvfile:
     routes = DictReader(csvfile)
     for route in routes:
-        alias = route['alias']
-        description = route['description']
-        fixed_target = target.lower().replace(' ', '')
-        fixed_alias = alias.lower().replace(' ', '')
-        if (
-            not target
-            or alias.startswith(target)
-            or fixed_alias.startswith(fixed_target)
-            or target in description.lower()
-        ):
+        name = route['name']
+        if not query or match(query_parts, name):
             url = route['url']
-            icon = f'{alias.split()[0]}.png'
-            wf.newline(
-                title=alias, autocomplete=alias, subtitle=description, arg=url, icon=icon
-            )
+            icon = route.get('icon', '')
+            wf.newline(title=name, autocomplete=name, subtitle=url, arg=url, icon=icon)
 
 wf.send()
