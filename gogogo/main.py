497f0b6e7bf22908061c9488d3f973a90d389093
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-02-10 13:42:42 +0000

    Fix boundaries for words

--- gogogo/main.py
+++ gogogo/main.py
@@ -6,9 +6,11 @@ from pathlib import Path
 
 from workflow import Workflow
 
+BOUNDARY_SYMBOLS = r'[- ()]+'
+
 
 def match(query_parts, name):
-    name_parts = re.split(r'[- ]+', name.lower())
+    name_parts = re.split(BOUNDARY_SYMBOLS, name.lower())
     for qpart in query_parts:
         if not any(npart.startswith(qpart) for npart in name_parts):
             return False
@@ -17,7 +19,7 @@ def match(query_parts, name):
 
 # https://bit.ly/3wBRr4e
 query = unicodedata.normalize('NFC', sys.argv[1]).strip().lower()
-query_parts = re.split(r'[- ]+', query)
+query_parts = re.split(BOUNDARY_SYMBOLS, query)
 
 wf = Workflow(icons_path=Path(__file__).parent / 'img')
 f = Path(__file__).parent / 'routes.csv'
