91a4762790d866179a7844743790c6dd0bb2bb2c
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-02-16 20:44:39 +0000

    Fix minor issues

--- rename/main.py
+++ rename/main.py
@@ -5,14 +5,13 @@ import findersel
 import workflow
 
 wf = workflow.Workflow()
-basename = wf.get_var('basename')
-padsize = wf.get_var('padsize', cast=int)
+files = list(findersel.get_selected_files(sort=True))
+basename = wf.get_arg(1)
+padsize = len(str(len(files)))
 
-processed_files = 0
-for i, file_path in enumerate(findersel.get_selected_files(sort=True), start=1):
+for i, file_path in enumerate(files, start=1):
     f = Path(file_path)
     new_stem = f'{basename}{i:0{padsize}d}'
     f.rename(f.with_stem(new_stem))
-    processed_files += 1
 
-print(processed_files, end='')
+print(len(files), end='')
