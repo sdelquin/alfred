3b9105175186e32ac543eede5295e3f2513c725c
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-02-20 14:43:13 +0000

    Fix typo

--- marte/main.py
+++ marte/main.py
@@ -4,7 +4,7 @@ from prettyconf import config
 
 HOME_HOSTNAME = config('HOME_HOSTNAME', default='home')
 MARTE_EXTERNAL_URL = config('MARTE_EXTERNAL_URL', default='marte')
-MARTE_INTERNAL_URL = config('MARTE_EXTERNAL_URL', default='marte')
+MARTE_INTERNAL_URL = config('MARTE_INTERNAL_URL', default='marte')
 
 if socket.gethostname().startswith(HOME_HOSTNAME):
     print(MARTE_EXTERNAL_URL)
