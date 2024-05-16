d9456b1d9e8c7c4ca35f95196fad6a4346e1b390
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-05-16 23:41:47 +0100

    Fix ip issues

--- marte/main.py
+++ marte/main.py
@@ -2,11 +2,18 @@ import socket
 
 from prettyconf import config
 
-HOME_HOSTNAME = config('HOME_HOSTNAME', default='home')
+INTERNAL_NETWORK = config('INTERNAL_NETWORK')
 MARTE_EXTERNAL_URL = config('MARTE_EXTERNAL_URL', default='marte')
 MARTE_INTERNAL_URL = config('MARTE_INTERNAL_URL', default='marte')
 
-if socket.gethostname().startswith(HOME_HOSTNAME):
-    print(MARTE_EXTERNAL_URL)
-else:
+
+def getip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 80))
+    return s.getsockname()[0]
+
+
+if getip().startswith(INTERNAL_NETWORK):
     print(MARTE_INTERNAL_URL)
+else:
+    print(MARTE_EXTERNAL_URL)
