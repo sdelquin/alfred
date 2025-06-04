86bf6d4cce956cea742ca2dfa9f8c70075b322ac
Sergio Delgado Quintero <sdelquin@gmail.com>
2025-06-04 12:57:25 +0100

    Allow float numbers with commas

--- ivaigic/main.py
+++ ivaigic/main.py
@@ -7,7 +7,7 @@ from workflow import Workflow
 
 wf = Workflow(icons_path=Path(__file__).absolute().parent / 'img')
 
-iva_price = float(sys.argv[1].strip().lower())
+iva_price = float(sys.argv[1].strip().lower().replace(',', '.'))
 igic_price = iva_price / 1.21 * 1.07
 
 wf.newline(title=f'{igic_price:.2f}€', subtitle='Price with IGIC', arg=igic_price)
